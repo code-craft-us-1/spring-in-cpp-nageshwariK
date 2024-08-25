b761b0b27ecec728fcbbed83f8b1df8921c64c18
Nageshwari Kiran <104964696+nageshwariK@users.noreply.github.com>
2024-08-25 19:13:34 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,28 @@
 #include "stats.h"
+#include <algorithm>
+#include <numeric>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
-}
+    int main()
+    {
+        return 0;
+    }
+
+    Statistics::Stats Statistics::ComputeStatistics(const std::vector<double>& vec)
+    {
+        //Implement statistics here
+        int n = vec.size();
+        Stats stat;
+        if (n != 0)
+        {
+            // average of the vector elements 
+            double avg = std::accumulate(vec.begin(), vec.end(), 0.0) / n;
+            //std::cout << "Average: " << avg << endl;
+            stat.average = avg;
+            double max1 = *max_element(vec.begin(), vec.end());
+            stat.max = max1;
+            double min1 = *min_element(vec.begin(), vec.end());
+            stat.min = min1;
+        }
+        return stat;
+   
+  }
