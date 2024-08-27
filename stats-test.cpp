75a18d186ca904f43ee0cbccffdf8df931507737
Nageshwari Kiran <104964696+nageshwariK@users.noreply.github.com>
2024-08-27 11:09:35 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -12,7 +12,7 @@ TEST(Statistics, ReportsAverageMinMax) {
 }
 
 TEST(Statistics, AverageNaNForEmpty) {
-    auto computedStats = Statistics::ComputeStatistics({});
+    auto computedStats = Statistics::ComputeStatistics({NAN,(INFINITY-INFINITY),HUGE_VAL*0.0F,NAN});
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     EXPECT_TRUE(std::isnan(computedStats.average));
